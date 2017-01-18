088bbfea0247d18e67ad3b35b0b3727420cf0d8a
Derek Villars and Lawrence Eby <derek@>
2017-01-18 11:38:05 -0800

    Add new tests for qu, still in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,31 @@
-// var vowels = ['a', 'e', 'i', 'o', 'u']
-// var consonants = ['y','q', 'w', 'r', 't', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
-//
+
 var result;
 
 function translator(input){
-
   if (/^[aeiou]$/ig.test(input.charAt(0))) {
     result = input + "ay";
 
   } else if (/^[y]$/ig.test(input.charAt(0))) {
     var wordArray = input.split("");
 
-    for (var index = 0; /^[yqwrtpsdfghjklzxcvbnm]$/ig.test(input.charAt(index)); index += 1) {
+    for (var index = 0; /^[yqwrtpsdfghjklzxcvbnm]/ig.test(input.charAt(index)); index += 1) {
       wordArray.push(input[index]);
       wordArray.shift();
-      };
-      result = wordArray.join("") + "ay";
+    };
+    result = wordArray.join("") + "ay";
 
-  } else if (/^[qwrtpsdfghjklzxcvbnm]$/ig.test(input.charAt(0))) {
+  } else if (/^[qwrtpsdfghjklzxcvbnm]/ig.test(input.charAt(0))) {
     var wordArray = input.split("");
 
-    for (var index = 0; /^[qwrtpsdfghjklzxcvbnm]$/ig.test(input.charAt(index)); index += 1) {
+    for (var index = 0; /^[qwrtpsdfghjklzxcvbnm]/ig.test(input.charAt(index)); index += 1) {
       wordArray.push(input[index]);
       wordArray.shift();
-      };
-      result = wordArray.join("") + "ay";
-    } else {
+    };
+    result = wordArray.join("") + "ay";
+  } else {
 
   };
-    return result;
+  return result;
 };
 
 
@@ -44,6 +41,11 @@ $(function(){
 });
 
 
-
+// TESTING FOR QU
+// for (var index = 0; index < input.length; index += 1){
 //
-// input.charAt(0) === 'a' || input.charAt(0) === 'e' || input.charAt(0) === 'i' || input.charAt(0) === 'o' ||   input.charAt(0) === 'u'
+//   if (/(qu)/ig.test(input.slice(index, index + 1))) {
+//     newWord = input.slice(index + 2) + input.slice(index, index + 1);
+//   };
+// result = newWord + "ay";
+// };
