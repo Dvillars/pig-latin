53f71a98e32b17d1b1d99b533774d7448153fd9b
Derek Villars and Lawrence Eby <lawrence@>
2017-01-18 10:27:40 -0800

    Add loops for checking consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
-function translator(){
-  
+// var vowels = ['a', 'e', 'i', 'o', 'u']
+// var consonants = ['y','q', 'w', 'r', 't', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
+//
+
+
+function translator(input){
+  var result;
+  if (/^[aeiou]$/ig.test(input.charAt(0))) {
+    result = input + "ay";
+
+  } else if (/^[qwrtypsdfghjklzxcvbnm]$/ig.test(input.charAt(0))) {
+    var wordArray = input.split("");
+
+    for (var index = 0; index < wordArray.length; index += 1) {
+      if (/^[qwrtypsdfghjklzxcvbnm]$/ig.test(input.charAt(index))) {
+        wordArray.push(input[index]);
+        wordArray.shift();
+      } else {
+        return;
+      }
+    }
+
+    result = wordArray + "ay";
+
+    // var newWord = input.slice(2) + input.slice(0, 1);
+    // result = newWord + "ay"
+  } else {
+
+
+
+
+    // var newWord = input.slice(1) + input.charAt(0);
+    // result = newWord + "ay"
+  }
+  return result;
+
 };
 
 
@@ -9,7 +43,11 @@ $(function(){
     var input = $("#input").val();
     var result = translator(input);
 
-    $(".output").text(result);
+    $(".result").text(result);
 
   });
 });
+
+
+//
+// input.charAt(0) === 'a' || input.charAt(0) === 'e' || input.charAt(0) === 'i' || input.charAt(0) === 'o' ||   input.charAt(0) === 'u'
