60529e6e6e853b01f117b17cb01cbd55c26eca27
Derek Villars and Lawrence Eby <lawrence@>
2017-01-18 14:44:56 -0800

    Add functionality for multiple word strings

--- js/scripts.js
+++ js/scripts.js
@@ -2,43 +2,49 @@
 var result;
 
 function translator(input){
-  // Vowel Test
-  if (/^[aeiou]$/ig.test(input.charAt(0))) {
-    result = input + "ay";
-    // Y Test
-  } else if (/^[y]$/ig.test(input.charAt(0))) {
-    var wordArray = input.split("");
+  var inputArray = input.split(" ");
+  var stringArray = [];
 
-    for (var index = 0; /^[yqwrtpsdfghjklzxcvbnm]/ig.test(input.charAt(index)); index += 1) {
-      wordArray.push(input[index]);
-      wordArray.shift();
-    };
-    result = wordArray.join("") + "ay";
-
-  // Consonant Test
-  } else if (/^[qwrtpsdfghjklzxcvbnm]/ig.test(input.charAt(0))) {
-    var wordArray = input.split("");
-
-    for (var index = 0; /^[qwrtpsdfghjklzxcvbnm]/ig.test(input.charAt(index)); index += 1) {
-      // Q Test
-      if (/^[q]/ig.test(input.charAt(index)) && /^[u]/ig.test(input.charAt(index + 1))) {
+  inputArray.forEach(function(input){
+    // Vowel Test
+    if (/^[aeiou]/ig.test(input.charAt(0))) {
+      stringArray.push(input + "ay");
+      // Y Test
+    } else if (/^[y]/ig.test(input.charAt(0))) {
+      var wordArray = input.split("");
+      for (var index = 0; /^[yqwrtpsdfghjklzxcvbnm]/ig.test(input.charAt(index)); index += 1) {
         wordArray.push(input[index]);
-        wordArray.push(input[index + 1])
         wordArray.shift();
-        wordArray.shift();
-      } else {
-      wordArray.push(input[index]);
-      wordArray.shift();
-    };
-    };
-    result = wordArray.join("") + "ay";
+      };
+      var newWord = wordArray.join("") + "ay";
+      stringArray.push(newWord);
 
-  } else {
+      // Consonant Test
+    } else if (/^[qwrtpsdfghjklzxcvbnm]/ig.test(input.charAt(0))) {
+      var wordArray = input.split("");
+      for (var index = 0; /^[qwrtpsdfghjklzxcvbnm]/ig.test(input.charAt(index)); index += 1) {
+        // Q Test
+        if (/^[q]/ig.test(input.charAt(index)) && /^[u]/ig.test(input.charAt(index + 1))) {
+          wordArray.push(input[index]);
+          wordArray.push(input[index + 1])
+          wordArray.shift();
+          wordArray.shift();
+        } else {
+          wordArray.push(input[index]);
+          wordArray.shift();
+        };
+      };
 
-  };
+      var newWord = wordArray.join("") + "ay";
+      stringArray.push(newWord);
+
+    } else {
+
+    };
+  });
+  result = stringArray.join(" ");
   return result;
 };
-
 
 $(function(){
   $("#blank").submit(function(event){
